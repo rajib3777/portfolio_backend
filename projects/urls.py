2b697c314bf49bec6d2b34ea9f0fadf898a6c4ef
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:42:16 +0600

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from .views import (
+    ProjectListView, ProjectDetailView, ProjectCategoryListView,
+)
+
+urlpatterns = [
+    path("", ProjectListView.as_view()),
+    path("categories/", ProjectCategoryListView.as_view()),
+    path("<slug:slug>/", ProjectDetailView.as_view()),
+]
 
