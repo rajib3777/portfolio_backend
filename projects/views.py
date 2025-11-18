81cd08d5fd209a5769da65dc2fd08aab6283d8ad
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:03:07 +0600

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Project, ProjectCategory
+from .serializers import ProjectSerializer, ProjectCategorySerializer
 
-# Create your views here.
+class ProjectListView(generics.ListAPIView):
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        qs = Project.objects.all()
+        category = self.request.query_params.get("category")
+        search = self.request.query_params.get("search")
+        featured = self.request.query_params.get("featured")
+
+        if category:
+            qs = qs.filter(category__name__iexact=category)
+        if search:
+            qs = qs.filter(title__icontains=search)
+        if featured == "true":
+            qs = qs.filter(featured=True)
+
+        return qs
+
+
+class ProjectDetailView(generics.RetrieveAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    lookup_field = "slug"
+    permission_classes = [permissions.AllowAny]
+
+
+class ProjectCategoryListView(generics.ListAPIView):
+    queryset = ProjectCategory.objects.all()
+    serializer_class = ProjectCategorySerializer
+    permission_classes = [permissions.AllowAny]
