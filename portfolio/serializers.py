85da596cd92e792686e1172e8657f6f34ff17038
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 13:51:00 +0600

    Update serializers.py

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,69 +1,54 @@
 from rest_framework import serializers
 from .models import (
     HeroSection, AboutSection, SkillCategory, Skill,
-    ProjectCategory, Project, Experience, Education,
-    SocialLink, Stats
+    Experience, Education, SocialLink, Stats
 )
-
 
 class HeroSectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = HeroSection
-        fields = '__all__'
-        
-        
+        fields = "__all__"
+
+
 class AboutSectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = AboutSection
-        fields = '__all__'
-        
-        
-class SkillCategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SkillCategory
-        fields = '__all__'
-        
-    
+        fields = "__all__"
+
+
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = '__all__'
-        
-        
-class ProjectCategorySerializer(serializers.ModelSerializer):
+        fields = "__all__"
+
+
+class SkillCategorySerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
+
     class Meta:
-        model = ProjectCategory
-        fields = '__all__'
-        
-        
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
-        
-        
+        model = SkillCategory
+        fields = ["id", "name", "skills"]
+
+
 class ExperienceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Experience
-        fields = '__all__'
-        
-        
+        fields = "__all__"
+
+
 class EducationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Education
-        fields = '__all__'
-        
-        
+        fields = "__all__"
+
+
 class SocialLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = SocialLink
-        fields = '__all__'
-        
-        
+        fields = "__all__"
+
+
 class StatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Stats
-        fields = '__all__'
-        
-        
-
+        fields = "__all__"
