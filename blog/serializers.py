2d53bc1b7d51b8179ffa477360a08acdfd006517
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:44:18 +0600

    Update serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,1 +1,16 @@
+from rest_framework import serializers
+from .models import BlogPost, BlogCategory
+
+class BlogCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BlogCategory
+        fields = "__all__"
+
+
+class BlogPostSerializer(serializers.ModelSerializer):
+    category = BlogCategorySerializer(read_only=True)
+
+    class Meta:
+        model = BlogPost
+        fields = "__all__"
 
