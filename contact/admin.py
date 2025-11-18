50df0a3aa441813f818f281b972f1e5a590c78a6
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:49:20 +0600

    Update admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -4,5 +4,5 @@ from .models import ContactMessage
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ("subject", "email", "created_at", "is_read")
-    search_fields = ("subject", "email", "message")
     list_filter = ("is_read", "created_at")
+    search_fields = ("subject", "email", "message")
