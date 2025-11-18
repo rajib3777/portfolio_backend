5c3515767906a2e966f011161a096f23e479f4ad
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 13:18:11 +0600

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -11,15 +11,21 @@ class BlogCategory(models.Model):
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True, blank=True)
-    category = models.ForeignKey(BlogCategory, on_delete=models.SET_NULL, null=True, related_name='posts')
+    category = models.ForeignKey(
+        BlogCategory, related_name="posts",
+        on_delete=models.SET_NULL, null=True, blank=True
+    )
     content = models.TextField()
-    thumbnail = models.ImageField(upload_to='blog/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="blog/", blank=True, null=True)
     published_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        ordering = ["-published_at"]
 
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
