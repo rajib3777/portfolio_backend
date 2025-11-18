49b1c0b0c25a35bb5f957258f7f105d7189d1451
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 13:26:40 +0600

    Update models.py

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ResumeFile(models.Model):
+    """
+    Optional manual upload (যদি চাই future এ ready-made PDF upload করতে)
+    """
+    file = models.FileField(upload_to="resumes/", blank=True, null=True)
+
+    def __str__(self):
+        return "Uploaded Resume"
