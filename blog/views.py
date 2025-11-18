8eda0bb2208c8c342854469eb09c17d881789738
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 13:20:35 +0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost, BlogCategory
+from .serializers import BlogPostSerializer, BlogCategorySerializer
 
-# Create your views here.
+class BlogListView(generics.ListAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.request.query_params.get("category")
+        search = self.request.query_params.get("search")
+        if category:
+            qs = qs.filter(category__name__iexact=category)
+        if search:
+            qs = qs.filter(title__icontains=search)
+        return qs
+
+
+class BlogDetailView(generics.RetrieveAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "slug"
+
+
+class BlogCategoryListView(generics.ListAPIView):
+    queryset = BlogCategory.objects.all()
+    serializer_class = BlogCategorySerializer
