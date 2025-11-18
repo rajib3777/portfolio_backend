7897040b74fb7b04187c4f9d2e3846b44ae39396
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:45:16 +0600

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+from .views import BlogListView, BlogDetailView, BlogCategoryListView
+
+urlpatterns = [
+    path("", BlogListView.as_view()),
+    path("categories/", BlogCategoryListView.as_view()),
+    path("<slug:slug>/", BlogDetailView.as_view()),
+]
 
