a7082f2a9e1faa0b10aa8c9c63880ce67a4b9bbb
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:40:57 +0600

    Update serializers.py

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,1 +1,24 @@
+from rest_framework import serializers
+from .models import Project, ProjectCategory, ProjectImage
+
+class ProjectImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectImage
+        fields = ["id", "image"]
+
+
+class ProjectCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectCategory
+        fields = "__all__"
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    category = ProjectCategorySerializer(read_only=True)
+    images = ProjectImageSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = "__all__"
+        read_only_fields = ["slug", "created_at"]
 
