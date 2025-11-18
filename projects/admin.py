2fd65784336345b64bf67faf628526d4d91aff33
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:03:40 +0600

    Update admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Project, ProjectCategory, ProjectImage
 
-# Register your models here.
+class ProjectImageInline(admin.TabularInline):
+    model = ProjectImage
+    extra = 1
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "created_at", "featured")
+    list_filter = ("category", "featured")
+    search_fields = ("title", "description")
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [ProjectImageInline]
+
+admin.site.register(ProjectCategory)
