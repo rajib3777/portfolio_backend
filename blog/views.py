2daf93a2cfe5d990e806ac089cde7fd9b413b6b3
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:44:40 +0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,16 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import BlogPost, BlogCategory
 from .serializers import BlogPostSerializer, BlogCategorySerializer
 
 class BlogListView(generics.ListAPIView):
-    queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+    permission_classes = [permissions.AllowAny]
 
     def get_queryset(self):
-        qs = super().get_queryset()
+        qs = BlogPost.objects.all()
         category = self.request.query_params.get("category")
         search = self.request.query_params.get("search")
+
         if category:
             qs = qs.filter(category__name__iexact=category)
         if search:
@@ -21,8 +22,10 @@ class BlogDetailView(generics.RetrieveAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
     lookup_field = "slug"
+    permission_classes = [permissions.AllowAny]
 
 
 class BlogCategoryListView(generics.ListAPIView):
     queryset = BlogCategory.objects.all()
     serializer_class = BlogCategorySerializer
+    permission_classes = [permissions.AllowAny]
