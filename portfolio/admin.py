23b2ac9a981016969413a9134dcf47459e5e311a
Md. Rajibul Islam Shuvo <rajibulislam3777@gmail.com>
2025-11-18 14:01:16 +0600

    Update admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from .models import (
     HeroSection, AboutSection, SkillCategory, Skill,
-    ProjectCategory, Project, Experience, Education,
-    SocialLink, Stats
+    Experience, Education, SocialLink, Stats
 )
 
 admin.site.register(HeroSection)
 admin.site.register(AboutSection)
 admin.site.register(SkillCategory)
 admin.site.register(Skill)
-admin.site.register(ProjectCategory)
-admin.site.register(Project)
 admin.site.register(Experience)
 admin.site.register(Education)
 admin.site.register(SocialLink)
